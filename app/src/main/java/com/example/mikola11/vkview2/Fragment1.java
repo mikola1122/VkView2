31ae3967c93395e9ddfcbe565b259126e5d60ebe
mikola11 <mikola11@i.ua>
2015-07-03 18:57:04 +0300

    add change orientation

--- app/src/main/java/com/example/mikola11/vkview2/Fragment1.java
+++ app/src/main/java/com/example/mikola11/vkview2/Fragment1.java
@@ -1,5 +1,8 @@
 package com.example.mikola11.vkview2;
 
+import android.app.Notification;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,19 +10,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.ActionMenuView;
+
+import java.util.List;
 
 
 public class Fragment1 extends Fragment {
 
     private WebView myWebView;
 
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment1, null);
+        //задаю вебвюшці юерельку
         myWebView = (WebView) v.findViewById(R.id.webView);
         myWebView.getSettings().setJavaScriptEnabled(true);
-        myWebView.loadUrl("https://oauth.vk.com/authorize");
+        myWebView.getSettings().setSaveFormData(true);
+        myWebView.clearCache(true);
+        myWebView.loadUrl("https://oauth.vk.com/authorize?client_id=4980642&scope=friends,photos&redirect_uri=https://oauth.vk.com/blank.html&scope=12&display=mobile&v=5.34&response_type=token");
+
+
+
+
+
         return v;
     }
 }
--- app/src/main/java/com/example/mikola11/vkview2/MainActivity.java
+++ app/src/main/java/com/example/mikola11/vkview2/MainActivity.java
@@ -1,22 +1,37 @@
 package com.example.mikola11.vkview2;
 
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 
+import java.util.List;
+
 
 public class MainActivity extends FragmentActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
 
+
         Fragment frag1 = new Fragment1();
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.add(R.id.fragment1, frag1);
         ft.commit();
+
+
+//        Uri data = getIntent().getData();
+//        String scheme = data.getScheme(); // "http"
+//        String host = data.getHost(); // "twitter.com"
+//        List<String> params = data.getPathSegments();
+//        String first = params.get(0); // "status"
+//        String second = params.get(1);
 
     }
 }
