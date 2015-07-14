0a0477608d57a488770d309802c68ebcd062c74a
mikola11 <mikola11@i.ua>
2015-07-14 16:57:46 +0300

    Add FriendsList.

---
+++ app/src/main/java/com/example/mikola11/vkview2/Friend.java
@@ -0,0 +1,45 @@
+package com.example.mikola11.vkview2;
+
+
+public class Friend {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String photoLink;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhotoLink() {
+        return photoLink;
+    }
+
+    public void setPhotoLink(String photoLink) {
+        this.photoLink = photoLink;
+    }
+
+    public String getFullName(){
+        return firstName + " " + lastName;
+    }
+}
--- app/src/main/java/com/example/mikola11/vkview2/FriendsListFragment.java
+++ app/src/main/java/com/example/mikola11/vkview2/FriendsListFragment.java
@@ -3,9 +3,15 @@ package com.example.mikola11.vkview2;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class FriendsListFragment extends Fragment {
@@ -16,7 +22,28 @@ public class FriendsListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.friends_list_fragment, null);
 
+        List<Friend> friendList = new ArrayList<Friend>();
+        populateFriends(friendList);
+
+        RecyclerView recyclerView = (RecyclerView) v.findViewById(R.id.friendsListRecyclerView);
+        FriendsListAdapter friendsListAdapter = new FriendsListAdapter(friendList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        RecyclerView.ItemAnimator itemAnimator = new DefaultItemAnimator();
+
+        recyclerView.setAdapter(friendsListAdapter);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(itemAnimator);
 
         return v;
+
+    }
+
+    private void populateFriends(List<Friend> friendList) {
+        for (int i = 0; i < 55; i++){
+            Friend friend = new Friend();
+            friend.setFirstName("Friend");
+            friend.setLastName(Integer.toString(i));
+            friendList.add(friend);
+        }
     }
 }
